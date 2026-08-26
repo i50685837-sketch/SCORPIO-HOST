d252f441e5f1cc25a4c904588f20e981439dde4c
i50685837-sketch <i50685837@gmail.com>
2026-08-26 14:06:38 +0300

    Delete routes/wallet.js

--- routes/wallet.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const walletController = require('../controllers/walletController');
-const auth = require('../middleware/auth');
-
-router.get('/balance', auth, walletController.getWalletBalance);
-router.post('/withdraw', auth, walletController.requestWithdrawal);
-
-module.exports = router;
